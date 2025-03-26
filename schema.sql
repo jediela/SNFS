265c20420e5f4bf64d6884e89d7f7b38ae7474dd
Jed Antalan <jediel2424@gmail.com>
2025-03-26 15:13:59 -0400

    add dummy data to cars table

--- schema.sql
+++ schema.sql
@@ -6,3 +6,9 @@ CREATE TABLE IF NOT EXISTS cars (
     year INT NOT NULL,
     price NUMERIC(10,2) NOT NULL
 );
+
+-- Insert dummy data into cars table
+INSERT INTO cars (make, model, year, price) VALUES
+('Toyota', 'Camry', 2020, 24000.00),
+('Honda', 'Accord', 2019, 22000.00),
+('Ford', 'Mustang', 2021, 35000.00);
