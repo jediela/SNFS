1bb1f8457e8ff21a4a4b6b694642d844d85cde32
Jed Antalan <jediel2424@gmail.com>
2025-03-26 15:10:50 -0400

    add dev instructions in readme.md

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 
+
 def create_app():
     app = Flask(__name__)
 
     # Import and register the car routes Blueprint
     from app.routes.car_routes import car_bp
+
     app.register_blueprint(car_bp)
 
     # Register other blueprints similarly, e.g., user_routes, portfolio_routes, etc.
-    
+
     return app
